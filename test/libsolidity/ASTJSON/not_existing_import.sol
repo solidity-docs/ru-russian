933f373eb4c5f3a7a02f86235686c15a83614d63
Olga Hryshkevich <68444638+olganicegirl@users.noreply.github.com>
2022-05-09 16:29:56 -0400

    cleaned up

--- test/libsolidity/ASTJSON/not_existing_import.sol
+++
@@ -1,9 +0,0 @@
-import "notexisting.sol" as NotExisting;
-contract C is NotExisting.X
-{
-	NotExisting.SomeStruct public myStruct;
-	constructor() {}
-}
-
-// ----
-// failAfter: Parsed
--- test/libsolidity/syntaxTests/using/free_overloads_array.sol
+++
@@ -1,9 +0,0 @@
-function f(uint x, uint[] y) pure returns (uint) {
-    return x;
-}
-function f(uint x, uint y) pure returns (int) {
-    return x;
-}
-using {f} for uint;
-// ----
-// DeclarationError 7920: (138-139): Identifier not found or not unique.
